509ea29842c286f3b33093bd789a83546c0e2409
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-03-18 22:23:12 -0300

    Agregando el ejercicio 1

---
+++ patrones/ejercicioDemo/src/main/java/ar/edu/info/unlp/ejercicioDemo/Tweet.java
@@ -0,0 +1,10 @@
+package ar.edu.info.unlp.ejercicioDemo;
+
+public class Tweet {
+	private String texto;
+	
+	public Tweet(String texto) {
+		this.texto = texto;
+	}
+	
+}
---
+++ patrones/ejercicioDemo/src/main/java/ar/edu/info/unlp/ejercicioDemo/Usuario.java
@@ -0,0 +1,9 @@
+package ar.edu.info.unlp.ejercicioDemo;
+
+public class Usuario {
+	private String nombre;
+	
+	public Usuario(String nombre) {
+		this.nombre = nombre;
+	}
+}
